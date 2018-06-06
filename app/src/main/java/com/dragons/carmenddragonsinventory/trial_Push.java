880c66e5a0474adf1128e02f0cbf45a9a8883b83
alevijester <andrewjester92@gmail.com>
2018-06-06 14:45:40 -0600

    comment

--- app/src/main/java/com/dragons/carmenddragonsinventory/trial_Push.java
+++ app/src/main/java/com/dragons/carmenddragonsinventory/trial_Push.java
@@ -2,4 +2,6 @@ package com.dragons.carmenddragonsinventory;
 
 public class trial_Push {
     //sorry I'm not in class today guys. Have Fun!
+    //thats ok! have a good day and we'll see you later!
+
 }
