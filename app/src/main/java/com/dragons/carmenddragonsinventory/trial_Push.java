644b26865891f8bd181929b73d0028605c9372af
alevijester <andrewjester92@gmail.com>
2018-06-06 14:54:03 -0600

    messgae

--- app/src/main/java/com/dragons/carmenddragonsinventory/trial_Push.java
+++ app/src/main/java/com/dragons/carmenddragonsinventory/trial_Push.java
@@ -4,4 +4,5 @@ public class trial_Push {
     //sorry I'm not in class today guys. Have Fun!
     //thats ok! have a good day and we'll see you later!
     // You owe us a million donuts!
+    //and a dragon!
 }
