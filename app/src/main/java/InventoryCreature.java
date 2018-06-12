41a310cd177efdfff2864dcf8eb0f42dfd5f250d
alevijester <andrewjester92@gmail.com>
2018-06-12 14:49:07 -0600

    InventoryCreature class

---
+++ app/src/main/java/InventoryCreature.java
@@ -0,0 +1,81 @@
+public class InventoryCreature {
+
+    //member variables
+    private String name;
+    private String color;
+    private String item;
+    private float costToProduce;
+    private float listPrice;
+    private int stock;
+    private String imgFilePath;
+
+    //constructor
+    public InventoryCreature(String name, String color, String item, float costToProduce,
+                             float listPrice, int stock, String imgFilePath){
+        this.name = name;
+        this.color = color;
+        this.item = item;
+        this.costToProduce = costToProduce;
+        this.listPrice = listPrice;
+        this.stock = stock;
+        this.imgFilePath = imgFilePath;
+    }
+
+    //getters
+    public String getName() {
+        return name;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public String getItem() {
+        return item;
+    }
+
+    public String getImgFilePath() {
+        return imgFilePath;
+    }
+
+    public float getCostToProduce() {
+        return costToProduce;
+    }
+
+    public float getListPrice() {
+        return listPrice;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    //setters
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public void setItem(String item) {
+        this.item = item;
+    }
+
+    public void setImgFilePath(String imgFilePath) {
+        this.imgFilePath = imgFilePath;
+    }
+
+    public void setCostToProduce(float costToProduce) {
+        this.costToProduce = costToProduce;
+    }
+
+    public void setListPrice(float listPrice) {
+        this.listPrice = listPrice;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+}
