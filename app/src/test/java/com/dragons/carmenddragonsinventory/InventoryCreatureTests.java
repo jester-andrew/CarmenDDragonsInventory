3c7df34f5bd2b953589e40deb26c46e8638af24f
ColonelJN <nee09001@byui.edu>
2018-06-13 15:02:17 -0600

    Removed unecessary class, Inventory Creature moved to correct package, connected database

---
+++ app/src/test/java/com/dragons/carmenddragonsinventory/InventoryCreatureTests.java
@@ -0,0 +1,13 @@
+package com.dragons.carmenddragonsinventory;
+
+import org.junit.Test;
+
+public class InventoryCreatureTests {
+    @Test
+    void newCreaturetest(){
+        InventoryCreature newCreature = new InventoryCreature("test","black","star",2, 2, 10
+        ,"null");
+
+    }
+
+}
