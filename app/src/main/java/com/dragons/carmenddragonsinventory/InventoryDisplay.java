a83dd64b8848d8d4ba1622ecdafc2e0d58953bdf
makinley <makinleysneff@gmail.com>
2018-06-21 13:37:32 -0600

    Left side pull out menu added

---
+++ app/src/main/java/com/dragons/carmenddragonsinventory/InventoryDisplay.java
@@ -0,0 +1,13 @@
+package com.dragons.carmenddragonsinventory;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class InventoryDisplay extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_inventory_display);
+    }
+}
