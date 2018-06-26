0e2b7708035d7c8b96d95e2f4ee776c6cad67f27
makinley <makinleysneff@gmail.com>
2018-06-26 12:39:36 -0600

    Linked inv button to display inv

--- app/src/main/java/com/dragons/carmenddragonsinventory/Main2Activity.java
+++ app/src/main/java/com/dragons/carmenddragonsinventory/Main2Activity.java
@@ -1,10 +1,9 @@
 package com.dragons.carmenddragonsinventory;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
 import android.support.design.widget.NavigationView;
+import android.support.v4.app.FragmentManager;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -34,6 +33,7 @@ public class Main2Activity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
     }
 
     @Override
@@ -76,8 +76,10 @@ public class Main2Activity extends AppCompatActivity
 
         if (id == R.id.nav_camera) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
 
+        } else if (id == R.id.nav_gallery) {
+            Intent Displayinventory = new Intent(this, InventoryDisplayActivity.class);
+            startActivity(Displayinventory);
         } else if (id == R.id.nav_share) {
 
         } else if (id == R.id.nav_send) {
