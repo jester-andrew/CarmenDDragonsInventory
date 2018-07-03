9e237440f422c12696c7dd5a03c9b763104fecd4
makinley <makinleysneff@gmail.com>
2018-07-03 14:32:56 -0600

    Housekeeping

--- app/src/main/java/com/dragons/carmenddragonsinventory/The_Main_Activity.java
+++ app/src/main/java/com/dragons/carmenddragonsinventory/The_Main_Activity.java
@@ -48,7 +48,7 @@ public class The_Main_Activity extends AppCompatActivity
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main2, menu);
+        getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
 
