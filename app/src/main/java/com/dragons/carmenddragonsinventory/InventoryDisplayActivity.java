da9c7a916a4060d086c8539d16496d6d074fd533
ColonelJN <nee09001@byui.edu>
2018-07-18 13:50:05 -0600

    JAVADOC for InventoryDisplayActivity

--- app/src/main/java/com/dragons/carmenddragonsinventory/InventoryDisplayActivity.java
+++ app/src/main/java/com/dragons/carmenddragonsinventory/InventoryDisplayActivity.java
@@ -19,10 +19,13 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.Query;
 import com.squareup.picasso.Picasso;
 
+/**
+ * This activity is used to display the full category chosen from the
+ * main menu.
+ * @author Justen
+ */
 public class InventoryDisplayActivity extends AppCompatActivity {
-    /**
-     * This class provides the interface to see the inventory
-     */
+
     private RecyclerView.LayoutManager layoutManager;
     protected Query inventoryQuery;
     private RecyclerView recyclerView;
@@ -32,6 +35,10 @@ public class InventoryDisplayActivity extends AppCompatActivity {
     private Integer image_width;
 
 
+    /**
+     * Overrides the Activity on create and initializes variables
+     * @param savedInstanceState contains all the information from a closed instance in order to re-initialize it
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -49,11 +56,27 @@ public class InventoryDisplayActivity extends AppCompatActivity {
         recyclerView.setLayoutManager(layoutManager);
         attachRecyclerViewAdapter();
     }
+
+    /**
+     * this method creates a new adapter to allow connection to the RecyclerView and
+     * then attaches it.
+     */
     // probably needed for when Auth is implemented
     private void attachRecyclerViewAdapter(){
         final RecyclerView.Adapter adapter = newAdapter();
         recyclerView.setAdapter(adapter);
     }
+
+    /**
+     * this class is where the major labor of attaching this app to the database happens
+     * @return an instance of the class
+     *
+     * In order to make this more readable, individual function calls are included here.
+     *  the options class is a template that the adapter uses to choose where to place data
+     *  the .setQuery call uses the variable returned by setQuery to find the reference of
+     *  the Firebase Database section we wish to display, and the embedded class SnapshotParser
+     *  transfers the data from the reference into each instance of the InventoryCreature model class.
+     */
     protected FirebaseRecyclerAdapter<InventoryCreature, CreatureHolder> newAdapter(){
        FirebaseRecyclerOptions<InventoryCreature> options =
                new FirebaseRecyclerOptions.Builder<InventoryCreature>()
@@ -81,8 +104,12 @@ public class InventoryDisplayActivity extends AppCompatActivity {
        return new FirebaseRecyclerAdapter<InventoryCreature,CreatureHolder>(options) {
 
 
-
-
+           /**
+            * this method allows us to insert ViewHolders into the associated RecyclerView
+            * @param parent the recyclerview to populate
+            * @param viewType the view id of the layout to use with the CreatureHolder
+            * @return a new instance of the view container CreatureHolder
+            */
            @NonNull
            @Override
            public CreatureHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
@@ -147,6 +174,10 @@ public class InventoryDisplayActivity extends AppCompatActivity {
     }
 
 
+    /**
+     * this function takes in the string passed via the intent's extras parameters and uses it to create the necessary query for the database
+     * @param category a string of one of three values. it is compared with a hard coded value to create the correct Query
+     */
     public void setQuery(String category) {
         Log.i(TAG, "setQuery: "+ category + " is the value of the extras");
         if (category.equals("fantasy")){
